cf4df13026fb10114cad562d6a7eee9558037791
solomon-yaw-amalitech <solomon.adeklo@amalitech.org>
2023-05-29 13:05:53 -0800

    modified modified:   index.html         modified:   index.js         modified:   monsters.js

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-// I wish you good luck and happy coding 🥰🤠🥳🥳💯💯
+"use strict";
+import {monsters} from "./monsters.js";  //import api
+
+for(let monsterDiv of monsters)
+{
+    showMonster(monsterDiv) ; 
+             
+}
+
+
+//Function that shows monster
+function showMonster(monsterDiv){
+    const monster = document.createElement("div");
+    monster.className = "monster";
+    
+    const monsterimage = document.createElement("img");
+    monsterimage.alt =monsterDiv.name;
+    
+    monsterimage.src = `https://robohash.org/${monsterDiv.id}?set=set2"`;
+    
+    const monstername = document.createElement("p");
+    monstername.className ="name";
+    monstername.innerText =monsterDiv.name;
+    
+    
+    const monsteremail = document.createElement("p");
+    monsteremail.className = "email";
+    monsteremail.innerText = monsterDiv.email;
+    
+    monster.append(monsterimage,monstername,monsteremail);
+
+    const monstersDiv = document.querySelector(".monsters");
+    monstersDiv.append(monster);
+    
+    
+}
+
+ 
+
+//Error Message Function
+function ShowError404Message(){
+
+const monsterNotFoundDiv = document.createElement("div");
+monsterNotFoundDiv.className ="p-5 not-found";
+monsterNotFoundDiv.style.display ="none";
+
+const errorSpan = document.createElement("span");
+errorSpan.innerText ="404";
+
+const errorMessage =  document.createElement("h1");
+errorMessage.innerText = "🧟‍♂️ No Monster Found 🧟‍♂️";
+
+monsterNotFoundDiv.append(errorSpan,errorMessage);
+
+const monstersDiv = document.querySelector(".monsters");
+monstersDiv.append(monsterNotFoundDiv);
+}
